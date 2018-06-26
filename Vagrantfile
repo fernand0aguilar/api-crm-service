4bc507fe29c19c0fc7fac3bbd110c671b2837d4e
Fernando Aguilar <fernando@aguilar.net.br>
2018-06-26 17:20:24 -0300

    Adding Vagrantfile and Puppet Manifest

---
+++ Vagrantfile
@@ -0,0 +1,15 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+
+  config.vm.provision :puppet do |puppet|
+    current_dir = __dir__
+    puppet.manifests_path = current_dir
+    puppet.manifest_file = "lamp.pp"
+  end
+
+  config.vm.box = "ubuntu/trusty64"
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+
+end
---
+++ lamp.pp
@@ -0,0 +1,28 @@
+exec { "apt-update":
+command => "/usr/bin/apt-get update"
+}
+
+package { "apache2":
+require => Exec["apt-update"],
+ensure => installed,
+}
+
+service { "apache2":
+ensure => running,
+}
+
+package { "mysql-server":
+require => Exec["apt-update"],
+ensure => installed,
+}
+
+package { "php5":
+require => Exec["apt-update"],
+ensure => installed,
+}
+
+file { "/var/www/html/info.php":
+ensure => file,
+content => "<?php phpinfo(); ?>",
+require => Package["apache2"],
+}
