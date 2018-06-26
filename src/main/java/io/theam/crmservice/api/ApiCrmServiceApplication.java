b1c22ba76451047ea997b14ac54a5077a36ce226
Fernando Aguilar <fernando@aguilar.net.br>
2018-06-26 18:37:34 -0300

    Adding Spring Starter Project

---
+++ src/main/java/io/theam/crmservice/api/ApiCrmServiceApplication.java
@@ -0,0 +1,12 @@
+package io.theam.crmservice.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrmServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/io/theam/crmservice/api/ApiCrmServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package io.theam.crmservice.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiCrmServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
